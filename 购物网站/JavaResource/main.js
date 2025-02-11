e1e2c08e80bb0d28eb43f2455a0b62646778c543
MengJGG <1602749414@qq.com>
2025-02-11 18:56:36 +0800

    message

--- 购物网站/JavaResource/main.js
+++ 购物网站/JavaResource/main.js
@@ -1,4 +1,20 @@
 'use strict';
 
 
+// 从服务器获取数据, path为请求的路径
+async function fetchData(path) {
+    try {
+        const response = await fetch(path);
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
 
+
+// window.addEventListener('DOMContentLoaded', async () => {
+//     const data = await fetchData('../Resource/JSONData/food/xican.json');
+//     console.log(data);
+// });
