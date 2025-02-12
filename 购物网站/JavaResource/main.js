f9222a674a74b599ccdc1c2fa57ba5655a8fc568
MengJGG <1602749414@qq.com>
2025-02-12 19:06:53 +0800

    message

--- 购物网站/JavaResource/main.js
+++ 购物网站/JavaResource/main.js
@@ -9,10 +9,20 @@ const GoodsItemsForInit = [
                 "../Resource/JSONData/guitar/yamaha.json"]
     },
     {"food":["../Resource/JSONData/food/xican.json"]},
-    {"racket":["../Resource/JSONData/racket/gongjianxilie.json",
-               "../Resource/JSONData/racket/jiguangxilie.json",
-               "../Resource/JSONData/racket/tianfuxilie.json"
-    
+    {"racket":[
+        {
+            "Yonex":[
+                "../Resource/JSONData/racket/gongjianxilie.json",
+                "../Resource/JSONData/racket/jiguangxilie.json",
+                "../Resource/JSONData/racket/tianfuxilie.json"
+            ]
+        },
+        {
+            "Li-Ning": []
+        },
+        {
+            "Victor": []
+        }
     ]}
 ]
 
@@ -30,12 +40,12 @@ async function fetchData(path) {
 }
 
 
-// 初始化商品的数据（用于数据量较小的情况）
+// 使用搜索树初始化商品的数据（用于数据量较小的情况）
 // 返回字典形式的商品数据，如果出错则返回null
-function initGoodsData() {
+function initGoodsData(items) {
     try {
         for (let i = 0; i < items.length; i++) {
-            goodsData[items[i]] = fetchData()
+            
         }
 
     } catch (error) {
@@ -49,3 +59,24 @@ function initGoodsData() {
 //     const data = await fetchData('../Resource/JSONData/food/xican.json');
 //     console.log(data);
 // });
+
+
+// 注册接口
+// 当用户点击注册按钮时，调用该接口
+function API_register() {
+    const usernameInput = document.getElementsByName('username')[0];
+    const passwordInput = document.getElementsByName('password')[0];
+    // 预留方案，如果有email或者二次密码
+    // const email = document.getElementsByName('email')[0];
+    // const password_2 = document.getElementsByName('password2')[0];
+
+    // 验证用户输入的用户名是否已被注册
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    // 获取cookie
+    
+
+
+}
